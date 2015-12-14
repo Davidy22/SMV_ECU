ca865b97ae34e3da69d446ac354a5644563936b3
davidy22 <davidyang6us@gmail.com>
2015-12-14 23:02:47 +0800

    Merged new stuff from the files in the dropbox folder

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -4,9 +4,8 @@
 int incoming = 0;
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
-int risingEdgePin = 2;     // Change according to actual pin numbers
-// int fuelPumpPin = 52;
-int onSwitchPin = 48;
+int HES_Pin = 2;  // Hall effect sensor
+int PMP_Pin = 48; // Fuel pump
 int TPS_Pin = 3;  // Throttle position sensor
 int INJ_Pin = 32; // Fuel injector
 int IAT_Pin = 4;  // Intake air temperature
@@ -14,16 +13,20 @@ int ECT_Pin = 5;  // Engine temperature sensor
 int MAP_Pin = 11; // Manifold air pressure
 int OIN_Pin = 1;  // Oxygen sensor input
 int OHE_Pin = 36; // Oxygen heater, digital
-int BUP_Pin = 9;  //Buttons
-int BDN_Pin = 13;
+int BUP_Pin = 9;  // Button one
+int BDN_Pin = 13; // Button two
+
 int ECT_Val = 0;
 int TPS_Val = 0;
-int IAT_Val = 0;    // added recently
+int IAT_Val = 0;
 int MAP_Val = 0;
 int OIN_Val = 0;
 int BUP_Val = 0;
 int BDN_Val = 0;
+
 int RPM = 0;
+
+// Boundary values
 const int TPS_min = 100;  // Test should be added to check if TPS starts at > 140, or it's clogged open.
 const int TPS_max = 927;
 const int IAT_min = 0;    // temp value to be changed later
@@ -34,9 +37,6 @@ boolean err = false;
 
 unsigned long time;          // |
 unsigned long lastTime;      // | Used for rotation-per-minute calculations
-int lastInput;
-int currentInput;
-int halfRevolutions = 0;
 
 double logResult;
 int temperatureCalculator;
@@ -65,68 +65,41 @@ double error = 0; // Changed these to change the frequency that these are cycled
 
 const int sampleSize = 50;
 int RPMSample[sampleSize];
-int RPMSamplePoint = 0;
 long avg_RPM = 0;
 int numSamples = 0;
-long delta;
 
-int zeros = 0;
-
-int Up = 0;
-int Dn = 0;
-
-int temp;
-//
-//// Number of relevant intervals in each parameter. Current numbers are wild guesses.
-//const int LOADVALUES = 10;
-//const int RPMVALUES = 5;
-//const int TEMPVALUES = 10;
-//int loadIndex;
-//int RPMIndex;
-//int tempIndex;
-//// Arrays of delay timings for fuel injection.
-//int onDelayArray[LOADVALUES][RPMVALUES][TEMPVALUES];
-//int offDelayArray[LOADVALUES][RPMVALUES][TEMPVALUES];
-
-//max off delay = 115
-//min off delay = 60
 void toggleOutput(const int Pin, int& Delay, int OnDelay, int OffDelay, 
                   boolean& on, int& LastSampled) {
      // Toggles a pin every increment of time denoted by OffDelay and OnDelay
-  //detachInterrupt(2);
   Delay += millis() - LastSampled;
 
   LastSampled = millis();
-  //detachInterrupt(2);
   if (Delay >= OffDelay) {
     Delay = 0;
     digitalWrite(Pin, HIGH);
     delay(OnDelay);
     digitalWrite(Pin, LOW);
   }
-  //attachInterrupt(2, rpm, FALLING);
 }
 
 void rpm() {
-//  currentInput = analogRead(risingEdgePin); //Redundant now that we understand how to interrupt
-//  if (lastInput - currentInput > 400) {
-//    halfRevolutions++;
-//  }
-//  lastInput = currentInput;
-  
-    // Engine RPM calculations
-  halfRevolutions++;
-  if (halfRevolutions >= 2) {
-    RPM = 120000/(millis() - lastTime);
-    halfRevolutions = 0;
+    RPM = 60000/(millis() - lastTime);
     lastTime = millis();
-  }
+}
+
+double getThrottlePosition() { //gets throttle position based off of the percentage of throttle area open
+  double angle = (throttlePositionConversion * (analogRead(TPS_Pin)*voltageConversion -.84));
+  return sin(angle);
+}
+
+double getTemp(int Pin) {
+  return analogRead(Pin)*voltageConversion*(-19.451)+89.135;
 }
 
 //void buttonUp() {
 //  INJ_OnDelay++;
 //}
-//
+
 //void buttonDown() {
 //  INJ_OnDelay--;
 //}
@@ -140,9 +113,10 @@ void setup () {
   ECT_Val = analogRead(ECT_Pin);
   MAP_Val = analogRead(MAP_Pin);
   IAT_Val = analogRead(IAT_Pin);
-  OIN_Val = analogRead(OIN_Pin); // Write error checking
+  OIN_Val = analogRead(OIN_Pin); // Write error checking for this one
   lastTime = millis();
-  attachInterrupt(2, rpm, FALLING);
+  attachInterrupt(HES_Pin, rpm, FALLING);
+  
     // Error Checking
   if (ECT_Val < ECT_min || ECT_Val > ECT_max)
   {
@@ -177,7 +151,6 @@ void setup () {
   }
   
   Serial.print("All systems Check!");
-  //delay(5000);
   
   //Initialise average RPM calculation array.
   for (int i = 0; i < 100; i++) {
@@ -187,18 +160,18 @@ void setup () {
 //  pinMode(fuelPumpPin, OUTPUT);   //Pre-pressurise the fuel
   pinMode(INJ_Pin, OUTPUT);
   digitalWrite(INJ_Pin, LOW);
-  pinMode(onSwitchPin, OUTPUT);
+  pinMode(PMP_Pin, OUTPUT);
   
-  digitalWrite(onSwitchPin, HIGH);
+  digitalWrite(PMP_Pin, HIGH);
   delay(1000);
-  digitalWrite(onSwitchPin, LOW);
+  digitalWrite(PMP_Pin, LOW);
   
   Serial.begin(9600);
   
 }
 
 void loop () {
-//  digitalWrite(onSwitchPin, HIGH);
+//  digitalWrite(PMP_Pin, HIGH);
 if (millis() - lastTime > 450) {
     RPM = 0;
   }
@@ -219,45 +192,26 @@ if (millis() - lastTime > 450) {
 //  }
   
   //Temperature calculation  
-  ECT_Val = analogRead(ECT_Pin);
-  logResult = log(ECT_Val*2.75942);
-  temperatureCalculator = (1.0 / (.002606445974 + .000000944070347*logResult + .000001722147429*pow(logResult, 3)) - 273.15)*1.8 + 32;
+//  ECT_Val = analogRead(ECT_Pin);
+//  logResult = log(ECT_Val*2.75942);
+//  temperatureCalculator = (1.0 / (.002606445974 + .000000944070347*logResult + .000001722147429*pow(logResult, 3)) - 273.15)*1.8 + 32;
   
   // Turn on the system if flywheel is turning.
   lcd.setCursor(13, 0);
   if (RPM != 0) {
-    digitalWrite(onSwitchPin, HIGH);
+    digitalWrite(PMP_Pin, HIGH);
     INIT_on = true;
     lcd.write("ON ");
-    //toggleOutput(onSwitchPin, INIT_Delay, INIT_MaxDelay, INIT_on, INIT_LastSampled);
+    //toggleOutput(PMP_Pin, INIT_Delay, INIT_MaxDelay, INIT_on, INIT_LastSampled);
   } else {
     INIT_on = false;
-    digitalWrite(onSwitchPin, LOW);
+    digitalWrite(PMP_Pin, LOW);
     lcd.write("OFF");
   }
   
   // End engine RPM calculations
-  
-  // Begin debug code
-  lcd.setCursor(0, 0);
-  lcd.print("VAL ");
-  lcd.print(INJ_OnDelay);
-  lcd.print(" ");
-  lcd.print(INJ_OffDelay);
-  lcd.print("     ");
 
-//  OIN_Val = analogRead(OIN_Pin);
-  temp = digitalRead(21);
-  TPS_Val = analogRead(TPS_Pin);
-  lcd.setCursor(0, 1);
-  lcd.print("PIN ");
-  lcd.print(Des_RPM); //used to be ECT_Val
-  lcd.print("      ");
-  lcd.setCursor(10, 1);
-  lcd.print(avg_RPM);
-  lcd.print("      ");
-  // End debug code
-
+  TPS_Val = getThrottlePosition()*1024;
   INJ_OffDelay = INJ_MaxOffDelay - (TPS_Val - 190)/2;
   Des_RPM = 5000 + (TPS_Val - 190)*5;
   
@@ -291,43 +245,53 @@ if (millis() - lastTime > 450) {
   }
 
 
-//  // Tabulated delay variation code. Requires testing and known values
-//  MAP_Val = analogRead(MAP_Pin);
-//  loadIndex = 0; // come up with actual formulae later
-//  RPMIndex = 0;
-//  tempIndex = 0;
-//  // number manipulation to change readings into index numbers
-//  INJ_OnDelay = onDelayArray[loadIndex][RPMIndex][tempIndex];
-//  INJ_OffDelay = onDelayArray[loadIndex][RPMIndex][tempIndex];
-  
-  //Button controlled delay variation
-//  BUP_Val = analogRead(BUP_Pin);
-//  BDN_Val = analogRead(BDN_Pin);
-//  if (Up != 1) {
-//    if (BUP_Val == 0) {
-//      INJ_OnDelay++;
-//      Up = 1;
-//    }
-//  } else {
-//    if (BUP_Val != 0) {
-//      Up = 0;
-//    }
-//  }
-//  if (Dn != 1) {
-//    if (BDN_Val == 0) {
-//      INJ_OnDelay--;
-//      Dn = 1;
-//    }
-//  } else {
-//    if (BDN_Val != 0) {
-//      Dn = 0;
-//    }
-//  }
-
   if (INIT_on) {
     toggleOutput(INJ_Pin, INJ_Delay, INJ_OnDelay, INJ_OffDelay, INJ_on, INJ_LastSampled);
   } else {
     digitalWrite(INJ_Pin, LOW);
     INJ_on = false;
-  }  
+  }
+  
+  // Begin debug code
+  lcd.setCursor(0, 0);
+  lcd.print("VAL ");
+  lcd.print(INJ_OnDelay);
+  lcd.print(" ");
+  lcd.print(INJ_OffDelay);
+  lcd.print("     ");
+
+  lcd.setCursor(0, 1);
+  lcd.print("PIN ");
+  lcd.print(Des_RPM); //used to be ECT_Val
+  lcd.print("      ");
+  lcd.setCursor(10, 1);
+  lcd.print(avg_RPM);
+  lcd.print("      ");
+  
+  // Serial output
+  Serial.print("#S|TEXT|[ECT: ");
+  Serial.print(getTemp(ECT_Pin));
+  Serial.print(" IAT: ");
+  Serial.print(getTemp(IAT_Pin));
+  Serial.print(" MAP: ");
+  Serial.print(analogRead(MAP_Pin)*voltageConversion);
+  Serial.print(" HES: ");
+  Serial.print(analogRead(HES_Pin)*voltageConversion);
+  Serial.print(" TPS: ");
+  Serial.print(getThrottlePosition());
+  Serial.println("]#");
+
+  
+  Serial.print("#S|EXCEL|[");
+  Serial.print(getTemp(ECT_Pin));
+  Serial.print(",");
+  Serial.print(getTemp(IAT_Pin));
+  Serial.print(",");
+  Serial.print(analogRead(MAP_Pin)*voltageConversion);
+  Serial.print(",");
+  Serial.print(analogRead(HES_Pin)*voltageConversion);
+  Serial.print(",");
+  Serial.print(getThrottlePosition());
+  Serial.println("]#");
+  // End debug code
 }
